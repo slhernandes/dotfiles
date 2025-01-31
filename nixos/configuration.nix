a250eb357c4bb6ee7aecb97cd96e41d55bf90cc0
slhernandes01 <slhernandes01@icloud.com>
2025-01-31 23:40:54 +0100

    udpated configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -232,7 +232,7 @@
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+  system.copySystemConfiguration = false;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
