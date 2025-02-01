3e465c76bae258c1c2e0f2d3f17c6482ade56a9e
slhernandes01 <slhernandes01@icloud.com>
2025-02-02 00:21:07 +0100

    added unzip to config

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,12 +3,14 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
+    rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
   };
 
-  outputs = {self, nixpkgs, ...}: {
+  outputs = {self, nixpkgs, ...} @inputs: {
     nixosConfigurations = {
       samuelhernandes-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [ ./configuration.nix ];
       };
     };
