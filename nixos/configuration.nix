b50b584ec12604236b98546e5f12a935add92888
slhernandes01 <slhernandes01@icloud.com>
2025-02-02 01:20:48 +0100

    Changed nameservers

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -38,6 +38,11 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.nameservers = [ 
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
+
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
